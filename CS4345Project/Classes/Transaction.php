7fce8c8cb1e3f47c0a01abe8914f2c1b83a8ee96
jbmur_000 <jbmur_000@Jonathan-pc>
2014-11-18 17:54:23 -0600

    changes

--- CS4345Project/Classes/Transaction.php
+++ CS4345Project/Classes/Transaction.php
@@ -1,5 +1,6 @@
 <?php
 include '../db_conn.php';
+include 'Picture.php';
 class Transaction{
 
 	private $id;
@@ -13,7 +14,13 @@ class Transaction{
 	private $followUpTransId;
 
 	function __construct($employeeId, $pointOfContactId, $date, $followUpReq, $type, $resultInSale, $followUpTransId){
-		$this->setLname($lname);
+		$this->setEmployeeId($employeeId);
+		$this->setPointOfContactId($pointOfContactId);
+		$this->setDate($date);
+		$this->setFollowUpReq($followUpReq);
+		$this->setType($type);
+		$this->setResultInSale($resultInSale);
+		$this->setFollowUpTransId($followUpTransId);
 	}
 
 	function setID($id){
@@ -81,25 +88,65 @@ class Transaction{
 	}
 
 
-	function saveTransaction($transaction){
+	function saveTransaction(){
+		global $db;
 		try{
-			$db->execute("INSERT INTO TRANSACTION ('employeeId', 'pointOfContactId', 'date', 'followUpReq','type','resultInSale',
-					'followUpTransId') VALUES ('".$this->getEmployeeId()."', '".$this->getgetPointOfContactId()."', '".$this->getDate()
-					."', '".$this->getFollowUpReq()."','".$this->getType()."','".$this->getResultInSale()
-					."','".$this->getFollowUpTransId()."');
+			$stmt = $db->prepare("INSERT INTO TRANSACTION ('employeeId', 'pointOfContactId', 'date', 'followUpReq','type','resultInSale',
+					'followUpTransId') VALUES (:employeeId, :pointOfContactId, :date, :followUpReq, :type, :resultInSale)");
+			$employeeID = $this->getEmployeeId();
+			$stmt->bindParam(':employeeID', $employeeID, PDO::PARAM_STR);
+			$pointOfContactId = $this->getPointOfContactId();
+			$stmt->bindParam(':pointOfContactID', $pointOfContactID, PDO::PARAM_STR);
+			$date = $this->getDate();
+			$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+			$followUpReq = $this->getFollowUpReq();
+			$stmt->bindParam(':followUpReq', $followUpReq, PDO::PARAM_STR);
+			$type = $this->getType();
+			$stmt->bindParam(':type', $type, PDO::PARAM_STR);
+		    $resultInSale = $this->getResultInSale();
+		    $stmt->bindParam(':resultInSale', $resultInSale, PDO::PARAM_STR);
+			$followUpTransId = $this->getFollowUpTransId();
+			$stmt->bindParam(':followUpTransId', $followUpTransId, PDO::PARAM_STR);
+			$stmt->execute();
 		}
 		catch(Exception $e)
 		{
 			/*** if we are here, something has gone wrong with the database ***/
-			$message = 'We are unable to process your request. Please try again later';
+			echo $e;
 		}
 	}
 
-}
-
-function getTransactionByID($id){
-	$db->query('SELECT * FROM TRANSACTION WHERE id = '.$id, PDO::FETCH_INTO, $emp);
-	return $emp;
+	function updateTransaction($id, $employeeID, $pointOfContactId, $date, $followUpReq, $type, $resultInSale, $followUpTransId){
+		global $db;
+		try{
+			if(isset($_FILES['photo']['name']) && $_FILES['photo']['size'] > 0)
+				insertPicture($id);
+			$stmt = $db->prepare("UPDATE TRANSACTION SET employeeId = :employeeId, pointOfContactId = :pointOfContactId, date = :date, followUpReq = :followUpReq,
+					type = :type, resultInSale = :resultInSale, followUpTransId = :followUpTransId  WHERE id = :id");
+			$stmt->bindParam(':employeeId', $employeeID, PDO::PARAM_STR);
+			$stmt->bindParam(':pointOfContactId', $pointOfContactId, PDO::PARAM_STR);
+			$date = $db->quote($date);
+			$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+			$followUpReq = $db->quote($followUpReq);
+			$stmt->bindParam(':followUpReq', $followUpReq, PDO::PARAM_STR);
+			$type = $db->quote($type);
+			$stmt->bindParam(':type', $type, PDO::PARAM_STR);
+			$resultInSale = $db->quote($resultInSale);
+			$stmt->bindParam(':resultInSale', $resultInSale, PDO::PARAM_STR);
+			$followUpTransId = $db->quote($followUpTransId);
+			$stmt->bindParam(':id', $id, PDO::PARAM_STR);
+			$stmt->execute();
+		}
+		catch(Exception $e)
+		{
+			/*** if we are here, something has gone wrong with the database ***/
+			echo '<br>'.$e;
+		}
+	}
+	
+	function getTransactionByID($id){
+		$db->query('SELECT * FROM TRANSACTION WHERE id = '.$id, PDO::FETCH_INTO, $emp);
+		return $emp;
 }
 
 function insertTransaction(){
@@ -107,4 +154,5 @@ function insertTransaction(){
 					 $_POST['type'], $_POST['resultInSale'], $_POST['followUpTransId']);
 	$emp->saveTransaction($this);
 }
+
 ?>
