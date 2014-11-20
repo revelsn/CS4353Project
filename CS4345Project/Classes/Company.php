bd60f5e804108ff24dea8ea85334874d48c3afd3
ATRoate <atroate@gmail.com>
2014-11-20 16:56:33 -0600

    Update Company.php
    Added functions, edited functions

--- CS4345Project/Classes/Company.php
+++ CS4345Project/Classes/Company.php
@@ -35,7 +35,7 @@ class Company{
 		return $this->dateCreated;
 	}
 	
-	function saveCompany($comp){
+	function saveCompany(){
 		global $db;
 		try{
 			$stmt = $db->prepare("INSERT INTO COMPANY (compName, dateCreated) VALUES (:compName,:dateCreated)");
@@ -51,15 +51,57 @@ class Company{
 		}
 	}
 	
-	function getCompByID($id){
-	$db->query('SELECT * FROM COMPANY WHERE id = '.$id, PDO::FETCH_INTO, $comp);
-	return $comp;
+	function updateCompany($id, $compName, $dateCreated){
+		global $db;
+		try{
+			$stmt = $db->prepare("UPDATE POINTOFCONTACT SET compName = :compName, dateCreated = :dateCreated WHERE id = :id");
+			$compName = $db->quote($compName);
+			$stmt->bindParam(':compName', $compName, PDO::PARAM_STR);
+			$dateCreated = $db->quote($dateCreated);
+			$stmt->bindParam(':dateCreated', $dateCreated, PDO::PARAM_STR);
+			$stmt->bindParam(':id', $id, PDO::PARAM_STR);
+			$stmt->execute();
+		}
+		catch(Exception $e)
+		{
+			/*** if we are here, something has gone wrong with the database ***/
+			echo '<br>'.$e;
+		}
+		
+	function companyArrayToObject($id){
+		global $db;
+		$stmt = $db->prepare('SELECT * FROM COMPANY WHERE id = :id');
+		$stmt->bindParam(':id', $id, PDO::PARAM_STR);
+		$stmt->execute();
+		$comp = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		$this->compName = $comp['compId'];
+		$this->dateCreated = $comp['dateCreated'];
+		return $this;
 	}
 	
-	function insertCompany(){
-		$comp = new Company($_POST['compName'],  date('Y-m-d H:i:s',time()));
-		$comp->saveCompany($this);
+	function getPocByID($id){
+		global $db;
+		$stmt = $db->prepare('SELECT * FROM COMPANY WHERE id = :id');
+		$stmt->bindParam(':id', $id, PDO::PARAM_STR);
+		$stmt->execute();
+		$comp = $stmt->fetch(PDO::FETCH_ASSOC);
+	
+		/* We will wrap in in an array so we can use the foreach on the showEmployee page */
+		$tempArray['1'] = $comp;
+		return $tempArray;
 	}
-
+	
+	function insertPointOfContact(){
+		$comp = new PointOfContact($_POST['compName'],date('Y-m-d H:i:s',time()));
+		print_r($comp);
+		$comp->savePointOfContact();
+	} 
+	
+	function updatePointOfContact($id){
+		$comp = new Company('', '');
+		$comp->companyArrayToObject($id);
+		$comp->updateCompany($id, $_POST[compName], $_POST['dateCreated']);
+	}
 	
 ?>
